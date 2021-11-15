2c96d7e3422fb84046a903014bf64fcca68ebecc
alex <17307@bk.ru>
2021-11-15 11:30:06 +0400

    initial commit10

--- src/main/java/com/haulmont/testtask/TesttaskApplication.java
+++ src/main/java/com/haulmont/testtask/TesttaskApplication.java
@@ -3,6 +3,16 @@ package com.haulmont.testtask;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Веб-приложение реализует добавление, редактирование и удаление следующих
+ * сущностей: банк, клиент, кредит, кредитное предложение, процесс оформления кредита
+ * на клиента с созданием графика платежей и расчетом необходимых сумм,
+ * автоматический расчет итоговой суммы процентов по кредиту,
+ * автоматический расчет суммы ежемесячного платежа с учетом процентной ставки.
+ *
+ * * @author Alexander Kolchenko
+ * * @version 1.01 14.11.2021
+ */
 @SpringBootApplication
 public class TesttaskApplication {
 
