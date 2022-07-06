f909c224d9a34f82f1ad8987199a7e00464e6930
new creator <as@mail>
2022-07-06 15:51:47 +0400

    new s

---
+++ src/main/java/com/haulmont/testtask/controllers/HealthController.java
@@ -0,0 +1,26 @@
+package com.haulmont.testtask.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    int i;
+
+    String s;
+
+    @PersistenceContext
+    protected EntityManager entityManager;
+
+    @GetMapping
+    public boolean healthz() {
+        return entityManager.createQuery("select 1", Boolean.class).getSingleResult();
+    }
+}
+
