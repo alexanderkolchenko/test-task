78c27b981a7a360c136612539d2c48dddc171882
akolchenko <alexander.kolchenko@artezio.com>
2022-08-30 00:03:39 +0400

    debug add and delete entities

---
+++ src/main/java/com/haulmont/testtask/exception_handler/IncorrectData.java
@@ -0,0 +1,16 @@
+package com.haulmont.testtask.exception_handler;
+
+public class IncorrectData {
+    private String info;
+
+    public IncorrectData() {
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
